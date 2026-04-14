cf679d947e1e083021f50582f840c9c6e00d4967
Ra-jin <hajin0335@gmail.com>
2026-04-14 15:17:51 +0900

    V22

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_rWjwMliCR', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4he8i58d2am9em70ndn6spep1r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2smbhoaubosjch7fd71snu369f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-notheast-2' // e.g. us-east-2
     },
     api: {
