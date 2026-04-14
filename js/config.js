d0693dabfe37b5584f22289bb105c70ef205ee1e
Ra-jin <hajin0335@gmail.com>
2026-04-14 14:51:47 +0900

    V2

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_15nDMvr2K', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7qjniknkfqefdlmrmvr0m7cu0e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        userPoolId: 'ap-northeast-2_rWjwMliCR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4he8i58d2am9em70ndn6spep1r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-notheast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://miq87fm8ef.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
